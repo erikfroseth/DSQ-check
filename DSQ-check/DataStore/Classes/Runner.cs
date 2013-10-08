5547080cf675c11c63154da84983d10a057cdb90
Erik Frøseth <erik@froseth.org>
2013-10-08 13:21:04 +0200

    Første versjon av løper-klassen

--- DSQ-check/DataStore/Classes/Runner.cs
+++ DSQ-check/DataStore/Classes/Runner.cs
@@ -5,7 +5,79 @@ using System.Text;
 
 namespace DSQ_check.DataStore.Classes
 {
-    class Runner
+    public class Runner
     {
+        private string _firstName, _lastName, _courseId, _clubName;
+        private uint? _ecard = null, _emitag1 = null, _emitag2 = null, _startNo = null;
+        public Runner(string firstName, string lastName, string courseId, string clubName, uint? ecard, uint? emitag1, uint? emitag2, uint? startno)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _courseId = courseId;
+            _clubName = clubName;
+
+            _ecard = ecard;
+            _emitag1 = emitag1;
+            _emitag2 = emitag2;
+            _startNo = startno;
+        }
+
+        public string ClubName
+        {
+            get
+            {
+                return _clubName;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+        }
+        public string CourseId
+        {
+            get
+            {
+                return _courseId;
+            }
+        }
+        public uint? Ecard
+        {
+            get
+            {
+                return _ecard;
+            }
+        }
+        public uint? EmiTag1
+        {
+            get
+            {
+                return _emitag1;
+            }
+        }
+        public uint? EmiTag2
+        {
+            get
+            {
+                return _emitag2;
+            }
+        }
+        public uint? StartNumber
+        {
+            get
+            {
+                return _startNo;
+            }
+        }
+
     }
 }
